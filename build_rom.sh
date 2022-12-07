8fed0888ddd33936ea6a979b38c96bfbbe2eedd3
RooGhz720 <Rooghz720@gmail.com>
2022-12-07 14:03:19 +0700

    testing5

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RooGhz720/Local-Manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/donboruza/local_manifests.git --depth 1 -b cherrish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch banana_sweet-userdebug
+lunch cherish_sweet-userdebug
+export BUILD_USERNAME=RooGhz720
+export BUILD_HOSTNAME=cei
 export TZ=Asia/Jakarta #put before last build command
-export KBUILD_BUILD_USER=RooGhz720
-export KBUILD_BUILD_HOST=android-runner
-mka sweet
+brunch sweet
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
