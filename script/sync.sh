5026b3a2af95b09ecbf17c81e738bd982aac03a1
SpesUnderworld <144889214+SpesUnderworld@users.noreply.github.com>
2023-09-19 20:19:30 +0530

    Update sync.sh

--- script/sync.sh
+++ script/sync.sh
@@ -4,7 +4,7 @@ set -exv
 name_rom=$(grep init $CIRCLE_WORKING_DIRECTORY/build.sh -m 1 | cut -d / -f 4)
 mkdir -p "$HOME/$WORKDIR/rom/$name_rom"
 cd $HOME/$WORKDIR/rom/$name_rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b sweet-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
